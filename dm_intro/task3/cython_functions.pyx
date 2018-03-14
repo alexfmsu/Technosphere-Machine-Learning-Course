01e8246baedd43bff37118b834fa443c0fc33bac
alexfmsu <alexfmsu@mail.ru>
2018-03-14 03:08:04 +0300

    dm_intro: clean code

--- dm_intro/task3/cython_functions.pyx
+++ dm_intro/task3/cython_functions.pyx
@@ -1,5 +1,7 @@
-import numpy as np
+from math import sqrt
 
+import numpy as np
+cimport numpy as np
 
 cpdef matrix_multiply(X, Y):
     """ Matrix multiplication
@@ -9,7 +11,25 @@ cpdef matrix_multiply(X, Y):
     Output:
       - out: A numpy array of shape (N, K)
     """
-    out = None
+    if X.shape[1] != Y.shape[0]:
+        print("Error: size mismatch")
+        exit(1)
+
+    cdef np.int N = X.shape[0]
+    cdef np.int M = Y.shape[0]
+    cdef np.int K = Y.shape[1]
+
+    cdef np.ndarray[np.float32_t, ndim = 2] out = np.zeros((N, K), dtype=np.float32)
+
+    for i in range(N):
+        for j in range(K):
+            val = .0
+
+            for k in range(M):
+                val += X[i, k] * Y[k, j]
+
+            out[i, j] = val
+
     return out
 
 
@@ -24,8 +44,51 @@ cpdef matrix_rowmean(X, weights=np.empty(0)):
     Output:
       - out: A numpy array of shape (N,)
     """
-    out = None
+    M = X.shape[0]
+    N = X.shape[1]
+
+    if weights.shape == (0,):
+        weights = np.ones(N, dtype=np.float32)
+
+    cdef np.float32_t w = .0
+
+    for i in range(N):
+        w += weights[i]
+
+    cdef np.ndarray[np.float32_t, ndim = 1] out = np.zeros(M, dtype=np.float32)
+
+    for i in range(M):
+        val = .0
+
+        for j in range(N):
+            if weights is None:
+                val += X[i, j]
+            else:
+                val += X[i, j] * weights[j]
+
+        out[i] = val / w
+
     return out
+
+cpdef array_mean(np.ndarray[np.float32_t, ndim=1] X):
+    cdef np.float32_t out = float(0)
+
+    for i in range(len(X)):
+        out += X[i]
+
+    out /= len(X)
+
+    return out
+
+cpdef array_std(np.ndarray[np.float32_t, ndim=1] X):
+    cdef np.float32_t mean = array_mean(X)
+
+    cdef np.ndarray[np.float32_t, ndim = 1] out = np.zeros(len(X), dtype=np.float32)
+
+    for i in range(len(X)):
+        out[i] = (X[i] - mean) ** 2
+
+    return sqrt(array_mean(out))
 
 
 cpdef cosine_similarity(X, top_n=10, with_mean=True, with_std=True):
@@ -52,5 +115,56 @@ cpdef cosine_similarity(X, top_n=10, with_mean=True, with_std=True):
         X = array([[ 1.,  0.], [ 0.,  1.]])
 
     """
-    out = None
+    M = X.shape[0]
+    N = X.shape[1]
+
+    cdef np.ndarray[np.float32_t, ndim = 2] x_tmp = np.zeros((M, N), dtype=np.float32)
+
+    cdef np.ndarray[np.float32_t, ndim = 1] x_row = np.zeros(X.shape[1], dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim = 1] x_col = np.zeros(M, dtype=np.float32)
+
+    cdef np.ndarray[np.float32_t, ndim = 2] out = np.zeros((M, M), dtype=np.float32)
+
+    for i in range(M):
+        for j in range(N):
+            x_tmp[i, j] = X[i, j]
+
+    if with_mean:
+        for i in range(M):
+            for j in range(N):
+                x_row[j] = x_tmp[i, j]
+
+            for j in range(N):
+                x_tmp[i, j] -= array_mean(x_row)
+
+    if with_std:
+        for i in range(M):
+            for j in range(N):
+                x_row[j] = x_tmp[i, j]
+
+            for j in range(N):
+                x_tmp[i, j] /= array_std(x_row)
+
+    for j in range(M):
+        for p in range(N):
+            x_row[p] = x_tmp[j, p]
+
+        args = np.argsort(x_row)
+
+        for i in range(N - top_n):
+            x_tmp[j, args[i]] = .0
+
+    for i in range(M):
+        for j in range(N):
+            x_col[i] += x_tmp[i, j] ** 2
+
+        x_col[i] = sqrt(x_col[i])
+
+    for i in range(M):
+        for j in range(M):
+            for k in range(N):
+                out[i, k] += x_tmp[i, k] * x_tmp[j, k]
+
+            out[i, j] /= x_col[i] * x_col[j]
+
     return out
